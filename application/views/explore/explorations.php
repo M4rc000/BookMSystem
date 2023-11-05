eab063ac49ed632783aaf9469605d893ee62cfb8
RobbyArsyadani <robbyarsyadani23@gmail.com>
2023-11-05 13:52:54 +0700

    l

--- application/views/explore/explorations.php
+++ application/views/explore/explorations.php
@@ -1,5 +1,15 @@
 <div class="content-wrapper" style="background-color: rgb(237,237,237);">
 	<div class="container">
+	<div class="form-group">
+		<div class="input-group d-flex justify-content-end">
+		  <input type="text" class="rounded w-25 mr-1 pl-3 border border-primary " placeholder="Search">
+		  <select class="form-select rounded mr-1 ho" id="inputGroupSelect01">
+			<option value="public">Public</option>
+			<option value="private">Private</option>
+		  </select>
+		  <button class="btn btn-primary" type="button" id="search-button">Search</button>
+		</div>
+	</div>
 		<div class="row row-cols-1 row-cols-md-3 g-4 mt-5 mb-3">
 			<div class="col">
 				<div class="card shadow" style="border-radius: 20px 20px 5px 5px !important;">
