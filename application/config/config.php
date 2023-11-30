967159d7a37f6a37d5a72c629ad53550d65e7b22
RobbyArsyadani <robbyarsyadani23@gmail.com>
2023-11-30 21:49:43 +0700

    bms

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/2023/BookMSystem/';
+$config['base_url'] = 'http://localhost/BMS/BookMSystem/';
 
 /*
 |--------------------------------------------------------------------------
@@ -378,8 +378,8 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'bms_session';
-$config['sess_expiration'] = 900;
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
 $config['sess_save_path'] = null;
 $config['sess_match_ip'] = false;
 $config['sess_time_to_update'] = 300;
--- bms-app.sql
+++ bms-app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 15, 2023 at 05:32 PM
+-- Generation Time: Nov 21, 2023 at 04:13 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -258,103 +258,6 @@ INSERT INTO `countries` (`country_id`, `country_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `menu`
---
-
-CREATE TABLE `menu` (
-  `menu_id` int(11) NOT NULL,
-  `menu_name` varchar(10) NOT NULL,
-  `menu_url` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `menu`
---
-
-INSERT INTO `menu` (`menu_id`, `menu_name`, `menu_url`) VALUES
-(1, 'Admin', 'admin'),
-(2, 'User', 'user'),
-(3, 'Explore', 'explore');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `role`
---
-
-CREATE TABLE `role` (
-  `role_id` int(2) NOT NULL,
-  `role_name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `role`
---
-
-INSERT INTO `role` (`role_id`, `role_name`) VALUES
-(1, 'Administrator'),
-(2, 'User');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `role_access_menu`
---
-
-CREATE TABLE `role_access_menu` (
-  `role_access_menu_id` int(2) NOT NULL,
-  `role_id` int(11) NOT NULL,
-  `menu_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `role_access_menu`
---
-
-INSERT INTO `role_access_menu` (`role_access_menu_id`, `role_id`, `menu_id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 3),
-(4, 2, 2),
-(5, 2, 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `submenu`
---
-
-CREATE TABLE `submenu` (
-  `submenu_id` int(11) NOT NULL,
-  `menu_id` int(2) NOT NULL,
-  `submenu_name` varchar(20) NOT NULL,
-  `submenu_url` varchar(50) NOT NULL,
-  `submenu_icon` varchar(50) NOT NULL,
-  `is_active` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `submenu`
---
-
-INSERT INTO `submenu` (`submenu_id`, `menu_id`, `submenu_name`, `submenu_url`, `submenu_icon`, `is_active`) VALUES
-(1, 1, 'Dashboard', 'admin/', 'ti-dashboard', 1),
-(2, 1, 'Manage User', 'admin/manage_user', 'mdi mdi-account-multiple', 1),
-(3, 1, 'Manage Role', 'admin/manage_role', 'mdi mdi-account-key', 1),
-(4, 1, 'Manage Menu', 'admin/manage_menu', 'ti-layout-tab', 1),
-(5, 1, 'Manage Sub-menu', 'admin/manage_sub_menu', 'ti-view-list-alt', 1),
-(6, 1, 'Manage Books', 'admin/manage_books', 'mdi mdi-book-open-page-variant', 1),
-(7, 2, 'My Profile', 'user/', 'mdi mdi-account-card-details', 1),
-(8, 2, 'Change Password', 'user/change_password', 'ti-unlock', 1),
-(9, 3, 'My Books', 'explore/', 'ti-book', 1),
-(10, 3, 'Explorations', 'explore/explorations', 'mdi mdi-compass-outline', 1),
-(11, 3, 'Collaborations', 'explore/collaborations', 'mdi mdi-compass-outline', 1),
-(12, 3, 'Explorations', 'explore/explorations', 'mdi mdi-compass-outline', 0),
-(13, 3, 'Collaborations', 'explore/collaborations', 'mdi mdi-compass-outline', 0);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
@@ -381,7 +284,105 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`, `image`, `gender`, `place_of_birth`, `date_of_birth`, `country`, `date_joined`, `is_active`, `role_id`) VALUES
 (1, 'Marco', 'Prime', 'marcoantoniomadgaskar@gmail.com', '$2y$10$DMF.T6mKOkmOUmpuH9mTseHBoOtZj4hX2TVkt4B6DMieiROqdwXQi', 'default.webp', 'Male', 'Bogor', '03-11-2003', 'Indonesia', '22-10-2023 10:49:21', 1, 1),
 (2, 'Robby', 'Robbs', 'robbyarsyadani@gmail.com', '$2y$10$DMF.T6mKOkmOUmpuH9mTseHBoOtZj4hX2TVkt4B6DMieiROqdwXQi', 'default.webp', 'Male', '', '', 'Indonesia', '22-10-2023 10:51:35', 1, 1),
-(3, 'Hansen', 'Hnsz', 'Hansenhoswari@gmail.com', '$2y$10$DMF.T6mKOkmOUmpuH9mTseHBoOtZj4hX2TVkt4B6DMieiROqdwXQi', 'default.webp', 'Male', '', '', 'Indonesia', '22-10-2023 10:52:37', 1, 1);
+(3, 'Hansen', 'Hnsz', 'Hansenhoswari@gmail.com', '$2y$10$DMF.T6mKOkmOUmpuH9mTseHBoOtZj4hX2TVkt4B6DMieiROqdwXQi', 'default.webp', 'Male', '', '', 'Indonesia', '22-10-2023 10:52:37', 1, 1),
+(4, 'Ucup', 'cupcup', 'ucup@gmail.com', '$2y$10$y2l9OIanr3f62MzBOe5kA.ir.M6KcvH/CPa42kkzJwn7irjuXrBFO', 'default.webp', '', '', '', '', '16-11-2023 19:50:32', 1, 2),
+(5, 'Ucup', 'MSI5992643', 'marco@gmail.com', '12345', 'Marco7.jpg', NULL, NULL, NULL, '', '', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(2) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 2, 2),
+(5, 2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Explore');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(2) NOT NULL,
+  `role` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'User');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(2) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `url` varchar(50) NOT NULL,
+  `icon` varchar(50) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin/', 'ti-dashboard', 1),
+(2, 1, 'Manage User', 'admin/manage_user', 'mdi mdi-account-multiple', 1),
+(3, 1, 'Manage Role', 'admin/manage_role', 'mdi mdi-account-key', 1),
+(4, 1, 'Manage Menu', 'admin/manage_menu', 'ti-layout-tab', 1),
+(5, 1, 'Manage Sub-menu', 'admin/manage_sub_menu', 'ti-view-list-alt', 1),
+(6, 1, 'Manage Books', 'admin/manage_books', 'mdi mdi-book-open-page-variant', 1),
+(7, 2, 'My Profile', 'user', 'mdi mdi-account-card-details', 1),
+(8, 2, 'Change Password', 'user/change_password', 'ti-unlock', 1),
+(9, 3, 'My Books', 'explore', 'ti-book', 1),
+(10, 3, 'Explorations', 'explore/explorations', 'mdi mdi-compass-outline', 1),
+(11, 3, 'Collaborations', 'explore/collaborations', 'mdi mdi-compass-outline', 1),
+(12, 3, 'Explorations', 'explore/explorations', 'mdi mdi-compass-outline', 0),
+(13, 3, 'Collaborations', 'explore/collaborations', 'mdi mdi-compass-outline', 0);
 
 --
 -- Indexes for dumped tables
@@ -400,33 +401,33 @@ ALTER TABLE `countries`
   ADD PRIMARY KEY (`country_id`);
 
 --
--- Indexes for table `menu`
---
-ALTER TABLE `menu`
-  ADD PRIMARY KEY (`menu_id`);
-
---
--- Indexes for table `role`
---
-ALTER TABLE `role`
-  ADD PRIMARY KEY (`role_id`);
-
---
--- Indexes for table `role_access_menu`
---
-ALTER TABLE `role_access_menu`
-  ADD PRIMARY KEY (`role_access_menu_id`);
-
---
--- Indexes for table `submenu`
---
-ALTER TABLE `submenu`
-  ADD PRIMARY KEY (`submenu_id`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -446,34 +447,34 @@ ALTER TABLE `countries`
   MODIFY `country_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
 
 --
--- AUTO_INCREMENT for table `menu`
---
-ALTER TABLE `menu`
-  MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `role`
---
-ALTER TABLE `role`
-  MODIFY `role_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `role_access_menu`
---
-ALTER TABLE `role_access_menu`
-  MODIFY `role_access_menu_id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `submenu`
---
-ALTER TABLE `submenu`
-  MODIFY `submenu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
