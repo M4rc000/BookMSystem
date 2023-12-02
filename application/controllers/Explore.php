38730c6d4a45f26fa47596ad255ef2bb18478f11
Marco Antonio <marcoantoniomadgaskar@gmail.com>
2023-12-02 15:21:25 +0700

    set explore views

--- application/controllers/Explore.php
+++ application/controllers/Explore.php
@@ -12,63 +12,39 @@ class Explore extends CI_Controller {
 	
 	public function index()
 	{
-<<<<<<< HEAD
-                $data['title'] = 'My Books';
+                $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+                $data['countries'] = $this->db->get('countries')->result_array();
+                $data['menus'] = $this->uri->segment(1);
                 $data['books'] = $this->EModel->getAllBooks();
-
+                $data['title'] = 'My Books';
+        
                 $this->load->view('templates/header', $data);
                 $this->load->view('templates/navbar');   
                 $this->load->view('templates/sidebar');   
                 $this->load->view('explore/my_books', $data);
                 $this->load->view('templates/footer');
-=======
-                $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-                $data['countries'] = $this->db->get('countries')->result_array();
-                $data['menus'] = $this->uri->segment(1);
-        $data['title'] = 'My Books';
-        
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/navbar');   
-        $this->load->view('templates/sidebar');   
-        $this->load->view('explore/my_books', $data);
-        $this->load->view('templates/footer');
->>>>>>> 2a701f2ea57e624a69b494aebbdae5146a70aff7
 	}
     
 	public function explorations()
 	{
-<<<<<<< HEAD
+                $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+                $data['countries'] = $this->db->get('countries')->result_array();
+                $data['menus'] = $this->uri->segment(1);
                 $data['title'] = 'Explorations';
-                
+        
                 $this->load->view('templates/header', $data);
                 $this->load->view('templates/navbar');   
                 $this->load->view('templates/sidebar');   
                 $this->load->view('explore/explorations',$data);
                 $this->load->view('templates/footer');
-=======
-                $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-                $data['countries'] = $this->db->get('countries')->result_array();
-                $data['menus'] = $this->uri->segment(1);
-        $data['title'] = 'Explorations';
-        
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/navbar');   
-        $this->load->view('templates/sidebar');   
-        $this->load->view('explore/explorations',$data);
-        $this->load->view('templates/footer');
->>>>>>> 2a701f2ea57e624a69b494aebbdae5146a70aff7
 	}
 
 	public function collaborations()
 	{
-<<<<<<< HEAD
-                $data['title'] = 'Collaborations';
-=======
                 $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
                 $data['countries'] = $this->db->get('countries')->result_array();
                 $data['menus'] = $this->uri->segment(1);
-        $data['title'] = 'Collaborations';
->>>>>>> 2a701f2ea57e624a69b494aebbdae5146a70aff7
+                $data['title'] = 'Collaborations';
 
                 $this->load->view('templates/header', $data);
                 $this->load->view('templates/navbar');   
--- application/views/user/change_password.php
+++ application/views/user/change_password.php
@@ -1,10 +1,11 @@
 <div class="content-wrapper">
-<nav aria-label="breadcrumb">
+	<nav aria-label="breadcrumb">
 		<ol class="breadcrumb">
 			<li class="breadcrumb-item"><a><?= ucfirst($menus); ?></a></li>
 			<li class="breadcrumb-item active" aria-current="page"><?= $title; ?></li>
 		</ol>
 	</nav>
+	<br>
 	<div class="card shadow">
 		<div class="card-body">
 			<div class="container">
