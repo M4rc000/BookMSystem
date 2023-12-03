9f72092a27365dcbbeb3e6a4704e7fbc53b8a194
RobbyArsyadani <robbyarsyadani23@gmail.com>
2023-12-03 20:23:33 +0700

    tambah book reader

---
+++ application/controllers/book.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Book extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+	{
+       
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        
+         
+        $this->load->view('book/book_reader');
+       
+	}
+
+}
